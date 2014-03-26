a339fcda7dde43aed2c1404aea5ba89a79886b79
Ben Wells <mrbrwells@gmail.com>
2014-03-26 11:48:54 -0400

    added placeholders for first routes/actions

--- main.rb
+++ main.rb
@@ -3,5 +3,26 @@ require "sinatra/config_file"
 require 'rack-flash'
 
 class FlickrApp < Sinatra::Base
-  
+  register Sinatra::FormKeeper
+  register Sinatra::ConfigFile
+  use Rack::flash
+
+  configure do
+    set :environment, :production
+    enable :sessions
+    set :session_secret, "Session Secret for shotgun development"
+    set :protection, :except => :frame_options
+    config_file "config/settings.yml"
+  end
+
+  # initializer route
+
+  # View photos attached to application (main view)
+
+  # edit single photo info
+
+  #delete photo
+
+  #upload new photo
+
 end
