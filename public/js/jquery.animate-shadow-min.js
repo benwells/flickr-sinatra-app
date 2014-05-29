8bbd1c99cf8ac943b554528a909e953c3e937993
Brandon <brandon@gooddonegreat.com>
2014-05-29 14:47:29 -0400

    Photo desk is now working as per the demo. I still want to add in some more stuff.

--- public/js/jquery.animate-shadow-min.js
+++ public/js/jquery.animate-shadow-min.js
@@ -1,10 +1,231 @@
-/*!
- * Shadow animation jQuery-plugin
+/**!
+ * @preserve Shadow animation 1.11
  * http://www.bitstorm.org/jquery/shadow-animation/
- * Copyright 2010 Edwin Martin <edwin@bitstorm.org>
+ * Copyright 2011, 2013 Edwin Martin <edwin@bitstorm.org>
+ * Contributors: Mark Carver, Xavier Lepretre and Jason Redding
  * Released under the MIT and GPL licenses.
  */
 
-(function(e){function i(a,b,c){var d=[];a.c&&d.push("inset");typeof b.color!="undefined"&&d.push("#"+g(a.color[0]+c*(b.color[0]-a.color[0]))+g(a.color[1]+c*(b.color[1]-a.color[1]))+g(a.color[2]+c*(b.color[2]-a.color[2])));typeof b.left!="undefined"&&d.push(parseInt(a.left+c*(b.left-a.left))+"px "+parseInt(a.top+c*(b.top-a.top))+"px");typeof b.blur!="undefined"&&d.push(parseInt(a.blur+c*(b.blur-a.blur))+"px");typeof b.a!="undefined"&&d.push(parseInt(a.a+c*(b.a-a.a))+"px");return d.join(" ")}function g(a){a=
-parseInt(a).toString(16);return a.length==1?"0"+a:a}function h(a){var b,c,d={};if(b=/#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/.exec(a))c=[parseInt(b[1],16),parseInt(b[2],16),parseInt(b[3],16)];else if(b=/#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])/.exec(a))c=[parseInt(b[1],16)*17,parseInt(b[2],16)*17,parseInt(b[3],16)*17];else if(b=/rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(a))c=[parseInt(b[1]),parseInt(b[2]),parseInt(b[3])];if(b=/([0-9]+)(?:px)?\s+([0-9]+)(?:px)?(?:\s+([0-9]+)(?:px)?)?(?:\s+([0-9]+)(?:px)?)?/.exec(a))d=
-{left:parseInt(b[1]),top:parseInt(b[2]),blur:b[3]?parseInt(b[3]):undefined,a:b[4]?parseInt(b[4]):undefined};d.c=/inset/.test(a);d.color=c;return d}var f;e.each(["boxShadow","MozBoxShadow","WebkitBoxShadow"],function(a,b){a=e("body").css(b);if(typeof a=="string"&&a!=""){f=b;return false}});if(f)e.fx.step.shadow=function(a){if(!a.init){a.b=h(e(a.elem).css(f));a.end=e.extend({},a.b,h(a.options.curAnim.shadow));a.init=true}a.elem.style[f]=i(a.b,a.end,a.pos)}})(jQuery);
+jQuery(function($, undefined) {
+	/**
+	 * Check whether the browser supports RGBA color mode.
+	 *
+	 * Author Mehdi Kabab <http://pioupioum.fr>
+	 * @return {boolean} True if the browser support RGBA. False otherwise.
+	 */
+	function isRGBACapable() {
+		var $script = $('script:first'),
+		color = $script.css('color'),
+		result = false;
+		if (/^rgba/.test(color)) {
+			result = true;
+		} else {
+			try {
+				result = (color !== $script.css('color', 'rgba(0, 0, 0, 0.5)').css('color'));
+				$script.css('color', color);
+			} catch (e) {
+			}
+		}
+		$script.removeAttr('style');
+
+		return result;
+	}
+
+	$.extend(true, $, {
+		support: {
+			'rgba': isRGBACapable()
+		}
+	});
+
+	/*************************************/
+
+	// First define which property to use
+	var styles = $('html').prop('style');
+	var boxShadowProperty;
+	$.each(['boxShadow', 'MozBoxShadow', 'WebkitBoxShadow'], function(i, property) {
+		var val = styles[property];
+		if (typeof val !== 'undefined') {
+			boxShadowProperty = property;
+			return false;
+		}
+	});
+
+	// Extend the animate-function
+	if (boxShadowProperty) {
+		$['Tween']['propHooks']['boxShadow'] = {
+			get: function(tween) {
+				return $(tween.elem).css(boxShadowProperty);
+			},
+			set: function(tween) {
+				var style = tween.elem.style;
+				var p_begin = parseShadows($(tween.elem)[0].style[boxShadowProperty] || $(tween.elem).css(boxShadowProperty));
+				var p_end = parseShadows(tween.end);
+				var maxShadowCount = Math.max(p_begin.length, p_end.length);
+				var i;
+				for(i = 0; i < maxShadowCount; i++) {
+					p_end[i] = $.extend({}, p_begin[i], p_end[i]);
+					if (p_begin[i]) {
+						if (!('color' in p_begin[i]) || $.isArray(p_begin[i].color) === false) {
+							p_begin[i].color = p_end[i].color || [0, 0, 0, 0];
+						}
+					} else {
+						p_begin[i] = parseShadows('0 0 0 0 rgba(0,0,0,0)')[0];
+					}
+				}
+				tween['run'] = function(progress) {
+					var rs = calculateShadows(p_begin, p_end, progress);
+					style[boxShadowProperty] = rs;
+				};
+			}
+		};
+	}
+
+	// Calculate an in-between shadow.
+	function calculateShadows(beginList, endList, pos) {
+		var shadows = [];
+		$.each(beginList, function(i) {
+			var parts = [], begin = beginList[i], end = endList[i];
+
+			if (begin.inset) {
+				parts.push('inset');
+			}
+			if (typeof end.left !== 'undefined') {
+				parts.push(parseFloat(begin.left + pos * (end.left - begin.left)) + 'px '
+				+ parseFloat(begin.top + pos * (end.top - begin.top)) + 'px');
+			}
+			if (typeof end.blur !== 'undefined') {
+				parts.push(parseFloat(begin.blur + pos * (end.blur - begin.blur)) + 'px');
+			}
+			if (typeof end.spread !== 'undefined') {
+				parts.push(parseFloat(begin.spread + pos * (end.spread - begin.spread)) + 'px');
+			}
+			if (typeof end.color !== 'undefined') {
+				var color = 'rgb' + ($.support['rgba'] ? 'a' : '') + '('
+				+ parseInt((begin.color[0] + pos * (end.color[0] - begin.color[0])), 10) + ','
+				+ parseInt((begin.color[1] + pos * (end.color[1] - begin.color[1])), 10) + ','
+				+ parseInt((begin.color[2] + pos * (end.color[2] - begin.color[2])), 10);
+				if ($.support['rgba']) {
+					color += ',' + parseFloat(begin.color[3] + pos * (end.color[3] - begin.color[3]));
+				}
+				color += ')';
+				parts.push(color);
+			}
+			shadows.push(parts.join(' '));
+		});
+		return shadows.join(', ');
+	}
+
+	// Parse the shadow value and extract the values.
+	function parseShadows(shadow) {
+		var parsedShadows = [];
+		var parsePosition = 0;
+		var parseLength = shadow.length;
+
+		function findInset() {
+			var m = /^inset\b/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.inset = true;
+				parsePosition += m[0].length;
+				return true;
+			}
+			return false;
+		}
+		function findOffsets() {
+			var m = /^(-?[0-9\.]+)(?:px)?\s+(-?[0-9\.]+)(?:px)?(?:\s+(-?[0-9\.]+)(?:px)?)?(?:\s+(-?[0-9\.]+)(?:px)?)?/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.left = parseInt(m[1], 10);
+				parsedShadow.top = parseInt(m[2], 10);
+				parsedShadow.blur = (m[3] ? parseInt(m[3], 10) : 0);
+				parsedShadow.spread = (m[4] ? parseInt(m[4], 10) : 0);
+				parsePosition += m[0].length;
+				return true;
+			}
+			return false;
+		}
+		function findColor() {
+			var m = /^#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.color = [parseInt(m[1], 16), parseInt(m[2], 16), parseInt(m[3], 16), 1];
+				parsePosition += m[0].length;
+				return true;
+			}
+			m = /^#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.color = [parseInt(m[1], 16) * 17, parseInt(m[2], 16) * 17, parseInt(m[3], 16) * 17, 1];
+				parsePosition += m[0].length;
+				return true;
+			}
+			m = /^rgb\(\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\)/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.color = [parseInt(m[1], 10), parseInt(m[2], 10), parseInt(m[3], 10), 1];
+				parsePosition += m[0].length;
+				return true;
+			}
+			m = /^rgba\(\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\)/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsedShadow.color = [parseInt(m[1], 10), parseInt(m[2], 10), parseInt(m[3], 10), parseFloat(m[4])];
+				parsePosition += m[0].length;
+				return true;
+			}
+			return false;
+		}
+		function findWhiteSpace() {
+			var m = /^\s+/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsePosition += m[0].length;
+				return true;
+			}
+			return false;
+		}
+		function findComma() {
+			var m = /^\s*,\s*/.exec(shadow.substring(parsePosition));
+			if (m !== null && m.length > 0) {
+				parsePosition += m[0].length;
+				return true;
+			}
+			return false;
+		}
+		function normalizeShadow(shadow) {
+			if ($.isPlainObject(shadow)) {
+				var i, sColor, cLength = 0, color = [];
+				if ($.isArray(shadow.color)) {
+					sColor = shadow.color;
+					cLength = sColor.length;
+				}
+				for(i = 0; i < 4; i++) {
+					if (i < cLength) {
+						color.push(sColor[i]);
+					} else if (i === 3) {
+						color.push(1);
+					} else {
+						color.push(0);
+					}
+				}
+			}
+			return $.extend({
+				'left': 0,
+				'top': 0,
+				'blur': 0,
+				'spread': 0
+			}, shadow);
+		}
+		var parsedShadow = normalizeShadow();
+
+		while (parsePosition < parseLength) {
+			if (findInset()) {
+				findWhiteSpace();
+			} else if (findOffsets()) {
+				findWhiteSpace();
+			} else if (findColor()) {
+				findWhiteSpace();
+			} else if (findComma()) {
+				parsedShadows.push(normalizeShadow(parsedShadow));
+				parsedShadow = {};
+			} else {
+				break;
+			}
+		}
+		parsedShadows.push(normalizeShadow(parsedShadow));
+		return parsedShadows;
+	}
+});
